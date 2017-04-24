936d83631c503ce467c567aaea13c143662ddeb8
mrginch <mrginch@live.unc.edu>
2017-04-24 17:30:44 -0400

    integrating webgazer tracking

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
-function main(){
-	alert('ohhhhh hellooooo');
-	//webgazer.begin();
-	//webgazer.end();
+function main(){	
 	
-	getCurrentPrediction();
+	webgazer.setRegression("threadedRidge");
 	
-	alert('ayoooo');
 	
+	webgazer.setGazeListener(function(data, elapsedTime) {
+    if (data == null) {
+        return;
+    }
+    var xprediction = data.x; //these x coordinates are relative to the viewport 
+    var yprediction = data.y; //these y coordinates are relative to the viewport
+    console.log(elapsedTime); //elapsed time is based on time since begin was called
+	}).begin();
+	
+	webgazer.showPredictionPoints(true);
+	
+	
+
 }
 
 $(document).ready(main);
